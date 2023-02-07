fb941a1919e6edb805c17e88aa3978cae69ebfb1
Ryan <vellabonelloryan@gmail.com>
2023-02-07 15:40:40 +0800

    feat: newly registered user is assigned User role

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Role;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
@@ -42,6 +43,8 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+
+        $user->roles()->sync(Role::where('title', 'User')->first()->id);
 
         event(new Registered($user));
 

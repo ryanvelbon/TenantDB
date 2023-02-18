f971d5ef1be4d9a9a53926cb4986912bc54e215c
Ryan <vellabonelloryan@gmail.com>
2023-02-18 16:19:11 +0800

    test: additional feature testing for Property and Tenant

--- tests/Feature/PropertiesTest.php
+++ tests/Feature/PropertiesTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use App\Models\Property;
+use App\Models\User;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -23,5 +24,35 @@ class PropertiesTest extends TestCase
         $this->post('/properties', $property->toArray())->assertRedirect('login');
         $this->patch($property->path())->assertRedirect('login');
         $this->delete($property->path())->assertRedirect('login');
+    }
+
+    public function test_user_can_access_properties()
+    {
+        $this->signIn();
+
+        $this->get('/properties')->assertStatus(200);
+    }
+
+    public function test_a_user_can_retrieve_a_property()
+    {
+        $user = User::factory()->create();
+        $property = Property::factory()->create(['created_by' => $user->id]);
+
+        $response = $this->actingAs($user)->get($property->path());
+        $response->assertStatus(200);
+    }
+
+    public function test_a_user_cannot_retrieve_a_property_of_others()
+    {
+        $userA = User::factory()->create();
+        $userB = User::factory()->create();
+        $property = Property::factory()->create(['created_by' => $userB->id]);
+
+        print($userA);
+        print($userB);
+        print($property);
+
+        $response = $this->actingAs($userA)->get($property->path());
+        $response->assertStatus(404);
     }
 }
--- tests/Feature/TenantsTest.php
+++ tests/Feature/TenantsTest.php
@@ -2,15 +2,46 @@
 
 namespace Tests\Feature;
 
-use Tests\TestCase;
-use App\Models\Tenant;
-
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Inertia\Testing\Assert;
+use Tests\TestCase;
+use App\Models\Tenant;
+use App\Models\User;
 
 class TenantsTest extends TestCase
 {
     use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::factory()->create();
+        $this->user->assignRole('landlord');
+
+        $this->user->tenants()->createMany([
+            [
+                'first_name' => 'John',
+                'last_name' => 'Doe',
+                'email' => 'john@example.com',
+                'phone' => '123-456-6789',
+                // 'nationality' => 14,
+                'passport' => 251917371230,
+                'id_card' => 392683340027,
+                'dob' => '1957-05-10',
+            ], [
+                'first_name' => 'Mary',
+                'last_name' => 'Jane',
+                'email' => 'mary@example.com',
+                'phone' => '385-948-3391',
+                // 'nationality' => 42,
+                'passport' => 91231282129,
+                'id_card' => 47475741189,
+                'dob' => '1976-07-18',
+            ],
+        ]);
+    }
 
     public function test_guests_cannot_crud_tenants()
     {
@@ -30,12 +61,55 @@ class TenantsTest extends TestCase
         $this->assertTrue(false);
     }
 
+    public function test_a_landlord_can_view_tenants()
+    {
+        $this->actingAs($this->user)
+            ->get('/tenants')
+            ->assertInertia(fn (Assert $assert) => $assert
+                ->component('Frontend/Tenant/Index')
+                ->has('tenants.data', 2)
+                ->has('tenants.data.0', fn (Assert $assert) => $assert
+                    ->where('firstName', 'John')
+                    ->where('lastName', 'Doe')
+                )
+                ->has('tenants.data.1', fn (Assert $assert) => $assert
+                    ->where('firstName', 'Mary')
+                    ->where('lastName', 'Jane')
+                )
+            );
+    }
+
+    public function test_a_landlord_can_retrieve_a_tenant()
+    {
+        $this->assertTrue(false);
+    }
+
     public function test_a_landlord_can_update_a_tenant()
     {
         $this->assertTrue(false);
     }
 
     public function test_a_landlord_can_delete_a_tenant()
+    {
+        $this->assertTrue(false);
+    }
+
+    public function test_two_landlords_can_create_their_own_tenant_record_for_the_same_individual()
+    {
+        $this->assertTrue(false);
+    }
+
+    public function test_a_landlord_cannot_create_two_tenants_with_the_same_email()
+    {
+        $this->assertTrue(false);
+    }
+
+    public function test_a_landlord_cannot_create_two_tenants_with_the_same_passport()
+    {
+        $this->assertTrue(false);
+    }
+
+    public function test_a_tenant_requires_a_first_name()
     {
         $this->assertTrue(false);
     }

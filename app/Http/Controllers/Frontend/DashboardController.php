ac0143282aadd33e304f0d965cf09e110b5a9df2
Ryan <vellabonelloryan@gmail.com>
2023-02-23 14:08:47 +0700

    adds static HTML to dashboard for user stats

--- app/Http/Controllers/Frontend/DashboardController.php
+++ app/Http/Controllers/Frontend/DashboardController.php
@@ -9,6 +9,6 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Frontend/Dashboard');
+        return Inertia::render('Frontend/Dashboard/Index');
     }
 }
--- resources/js/Pages/Frontend/Dashboard/Index.vue
+++ resources/js/Pages/Frontend/Dashboard/Index.vue
@@ -1,6 +1,7 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
 import { Head } from '@inertiajs/vue3';
+import Stats from './Partials/Stats.vue'
 </script>
 
 <template>
@@ -11,10 +12,18 @@ import { Head } from '@inertiajs/vue3';
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
         </template>
 
-        <div class="py-12">
+        <div class="mt-2">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">You're logged in!</div>
+                </div>
+            </div>
+        </div>
+
+        <div class="mt-4">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="overflow-hidden shadow-sm sm:rounded-lg">
+                    <Stats />
                 </div>
             </div>
         </div>

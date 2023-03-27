8e9243f165c5037e12d00d176a063da682354456
Ryan <vellabonelloryan@gmail.com>
2023-03-27 12:21:50 +0800

    style: frontend.contracts.index page

--- resources/js/Pages/Frontend/Contract/Index.vue
+++ resources/js/Pages/Frontend/Contract/Index.vue
@@ -10,35 +10,74 @@ const props = defineProps({
 
 <template>
   <AppLayout>
-    <div>
-      <Link :href="route('frontend.contracts.create')" class="bg-blue-500 hover:bg-blue-300 p-2">Create contract</Link>
-    </div>
-    <table class="text-xs">
-      <thead>
-        <tr>
-          <th>Start</th>
-          <th>End</th>
-          <th>Tenant</th>
-          <th>Rent</th>
-          <th>Deposit</th>
-          <th>edit</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="contract in contracts.data" :key="contract.id">
-          <td class="px-4">{{ contract.startDate }}</td>
-          <td class="px-4">{{ contract.endDate }}</td>
-          <td class="px-4">{{ contract.tenant.first_name }} {{ contract.tenant.last_name }}</td>
-          <td class="px-4">{{ contract.rent }} / month</td>
-          <td class="px-4">{{ contract.deposit }}</td>
-          <td>
-            <Link :href="`/contracts/${contract.id}/edit`">edit</Link>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-    <div>
-      <Pagination class="mt-6" :links="contracts.links" />
+    <div class="px-4 sm:px-6 lg:px-8">
+      <div class="sm:flex sm:items-center">
+        <div class="sm:flex-auto">
+          <h1 class="text-xl font-semibold text-gray-900">Contracts</h1>
+          <p class="mt-2 text-sm text-gray-700">This is where you manage your lease agreements.</p>
+        </div>
+        <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+          <Link :href="route('frontend.contracts.create')" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">New Contract</Link>
+        </div>
+      </div>
+
+      <div class="mt-8 flex flex-col">
+        <div class="">
+          <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table class="min-w-full divide-y divide-gray-200">
+              <thead class="bg-gray-50">
+                <tr>
+                  <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Start
+                  </th>
+                  <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Ends
+                  </th>
+                  <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Tenant Name
+                  </th>
+                  <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Rent (USD)
+                  </th>
+                  <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Deposit (USD)
+                  </th>
+                  <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Actions
+                  </th>
+                </tr>
+              </thead>
+              <tbody class="bg-white divide-y divide-gray-200">
+                <tr v-for="contract in contracts.data" :key="contract.id">
+                  <td class="px-6 py-4 whitespace-nowrap">
+                    <div class="text-sm text-gray-900">{{ contract.startDate }}</div>
+                  </td>
+                  <td class="px-6 py-4 whitespace-nowrap">
+                    <div class="text-sm text-gray-900">{{ contract.endDate }}</div>
+                  </td>
+                  <td class="px-6 py-4 whitespace-nowrap">
+                    <div class="text-sm text-gray-900">{{ contract.tenant.first_name }} {{ contract.tenant.last_name }}</div>
+                  </td>
+                  <td class="px-6 py-4 whitespace-nowrap">
+                    <div class="text-sm text-gray-900">{{ contract.rent }}</div>
+                  </td>
+                  <td class="px-6 py-4 whitespace-nowrap">
+                    <div class="text-sm text-gray-900">{{ contract.deposit }}</div>
+                  </td>
+                  <td class="px-6 py-4 whitespace-nowrap text-left text-sm font-medium">
+                    <Link :href="`/contracts/${contract.id}/edit`">edit</Link>
+                    <a href="#" class="text-red-600 hover:text-red-900 ml-4">delete</a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <Pagination class="mt-6" :links="contracts.links" />
+      </div>
     </div>
   </AppLayout>
 </template>

57efde9b308c054f68815cd34ba8b03f4a259695
Ryan <vellabonelloryan@gmail.com>
2023-10-07 18:06:41 +0800

    adds filter scope to Tenant model

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -75,4 +75,17 @@ class Tenant extends Model
     {
         return $date->format('Y-m-d H:i:s');
     }
+
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['search'] ?? null, function ($query, $search) {
+            $query->where('first_name', 'like', '%'.$search.'%');
+        })->when($filters['trashed'] ?? null, function ($query, $trashed) {
+            if ($trashed === 'with') {
+                $query->withTrashed();
+            } elseif ($trashed === 'only') {
+                $query->onlyTrashed();
+            }
+        });
+    }
 }
